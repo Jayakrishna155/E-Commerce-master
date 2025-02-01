94b9b33340a17e7dc50e38d7c055761ef0bbb13e
JAYAKRISHNA <143500077+Jayakrishna155@users.noreply.github.com>
2025-02-01 21:16:26 +0530

    Update Item.jsx

--- frontend/src/Components/Items/Item.jsx
+++ frontend/src/Components/Items/Item.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
+
 export default function Item(props) {
+  // Replace localhost URL with the new backend URL
+  const updatedImageUrl = props.image.replace('http://localhost:4000/images/', 'https://e-commerce-backend-91i0.onrender.com/images/');
+
   return (
     <div className='item'>
-       <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-        <p>{props.name}</p>
-        <div className='item-prices'>
-            <div className="item-price-new">
-              ${props.new_price}
-            </div>
-            <div className="item-price-old">
-               ${props.old_price}
-            </div>
-
+      <Link to={`/product/${props.id}`}>
+        <img onClick={window.scrollTo(0, 0)} src={updatedImageUrl} alt="" />
+      </Link>
+      <p>{props.name}</p>
+      <div className='item-prices'>
+        <div className="item-price-new">
+          ${props.new_price}
         </div>
+        <div className="item-price-old">
+          ${props.old_price}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
