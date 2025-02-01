cca34ff9fe64f3686ff1bf0206c80cccfc79dcd7
Jayakrishna <jayakrishnayadav155@gmail.com>
2025-02-01 19:50:25 +0530

    breadcrum updated href=#

--- frontend/src/Components/Breadcrums/breadcrum.jsx
+++ frontend/src/Components/Breadcrums/breadcrum.jsx
@@ -6,7 +6,7 @@ export default function Breadcrum (props) {
     const {product} = props;
   return (
     <div className='breadcrum'>
-      <a> <Link style={{textDecoration:"none",color:"#626262"}} to="/">HOME</Link> </a> <img src={arrowIcon} alt="" />       <a> <Link style={{textDecoration:"none",color:"#626262"}} to="/">SHOP</Link> </a> <img src={arrowIcon} alt="" />      <a> <Link style={{textDecoration:"none",color:"#626262"}} to={`/${product.category}s`}>{product.category}</Link> </a> <img src={arrowIcon} alt="" />
+      <a href='#'> <Link style={{textDecoration:"none",color:"#626262"}} to="/">HOME</Link> </a> <img src={arrowIcon} alt="" />       <a href='#'> <Link style={{textDecoration:"none",color:"#626262"}} to="/">SHOP</Link> </a> <img src={arrowIcon} alt="" />      <a href='#'> <Link style={{textDecoration:"none",color:"#626262"}} to={`/${product.category}s`}>{product.category}</Link> </a> <img src={arrowIcon} alt="" />
       <span>{product?.name}</span>
        
     </div>
